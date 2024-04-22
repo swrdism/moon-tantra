8b9eb4765a36d9dc2a9b092026c1e71c24340453
swrdism <swrdism@gmail.com>
2024-04-22 12:09:15 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/moon-tantra',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
